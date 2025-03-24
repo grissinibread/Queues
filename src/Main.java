dc140f473a6d791a2dc9d9d823046d1d6101a696
Alejandro Navarro <alejandronavarro@Alejandros-MacBook-Pro-4.local>
2025-03-24 16:21:08 -0700

    Priority Queues

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Queue queue = new Queue(5);
+        PriorityQueue priorityQueue = new PriorityQueue();
 
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        queue.enqueue(40);
-        queue.enqueue(50);
+        priorityQueue.insert(1);
+        priorityQueue.insert(2);
+        priorityQueue.insert(4);
+        priorityQueue.insert(5);
 
 
-        queue.dequeue();
-        queue.enqueue(60);
-        queue.dequeue();
-        queue.dequeue();
-
-        System.out.println(queue);
-
-        QueueStack queueStack = new QueueStack();
-
-        queueStack.add(10);
-        queueStack.add(20);
-        queueStack.add(30);
-        queueStack.add(40);
-        queueStack.add(50);
-
-        queueStack.pop();
-        queueStack.add(60);
-        queueStack.pop();
-        queueStack.pop();
-
-        System.out.println(queueStack);
+        System.out.println(priorityQueue);
     }
 
 //    public static void reverse(Queue<Integer> queue) {
--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class PriorityQueue {
+    private int[] queue = new int[5];
+    private int count = 0; // = 0
+
+    public void insert(int element) {
+        if(isFull())
+            throw new IllegalStateException();
+
+        var current = shiftItemsToInsert(element);
+
+        this.queue[current] = element;
+        count++;
+    }
+
+    private int shiftItemsToInsert(int element) {
+        int current;
+
+        for(current = (this.count - 1); current >= 0; current--) {
+            if(this.queue[current] > element)
+                this.queue[current + 1] = this.queue[current];
+            else
+                break;
+        }
+        return current + 1;
+    }
+
+    public void dequeue() {
+        if(isEmpty())
+            throw new IllegalStateException();
+
+        this.queue[--this.count] = 0;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(this.queue);
+    }
+
+    private boolean isEmpty() {
+        return this.count == 0;
+    }
+
+    private boolean isFull() {
+        return this.count == this.queue.length;
+    }
 }
